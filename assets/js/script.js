988509734dbfb6a5c7e5685834ea524d27eb2289
lidalton <lidaltons@gmail.com>
2023-09-27 21:57:27 -0300

    create: general

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+  );
+  mobileNavbar.init();
